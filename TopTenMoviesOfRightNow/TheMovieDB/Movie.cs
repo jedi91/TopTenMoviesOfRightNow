95b113dfc815d4258088dac088304eb8894af537
Michael <mrkovacevich@gmail.com>
2017-09-07 20:31:18 -0700

    Removed unnecessary fields from Movie.

--- TopTenMoviesOfRightNow/TheMovieDB/Movie.cs
+++ TopTenMoviesOfRightNow/TheMovieDB/Movie.cs
@@ -10,19 +10,9 @@ namespace TopTenMoviesOfRightNow.TheMovieDB
     public class Movie
     {
         private string poster_path;
-        private bool adult;
         private string overview;
         private string release_date;
-        private int[] genre_ids;
-        private int id;
-        private string original_title;
-        private string original_language;
         private string title;
-        private string backdrop_path;
-        private decimal popularity;
-        private int vote_count;
-        private bool video;
-        private decimal vote_average;
 
         public string Title {
             get { return title; }
@@ -46,19 +36,9 @@ namespace TopTenMoviesOfRightNow.TheMovieDB
         public Movie(Result searchResult)
         {
             poster_path = searchResult.poster_path;
-            adult = searchResult.adult;
             overview = searchResult.overview;
             release_date = searchResult.release_date;
-            genre_ids = searchResult.genre_ids;
-            id = searchResult.id;
-            original_title = searchResult.original_title;
-            original_language = searchResult.original_language;
             title = searchResult.title;
-            backdrop_path = searchResult.backdrop_path;
-            popularity = searchResult.popularity;
-            vote_count = searchResult.vote_count;
-            video = searchResult.video;
-            vote_average = searchResult.vote_average;
         }
     }
 }
